beceb0d3bc0e1f66e589d57775328d453f64e442
tomohiro1024 <gongyezhihong732@gmail.com>
2023-06-28 10:24:12 +0900

    静的コンテンツの配信

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const PORT = process.env.PORT
+const path = require("path")
 const express = require("express")
 const app = express()
 
-// リクエストを受けたとき、どのような処理をするのか
+// expressのセッティング
 app.set("view engine", "ejs")
 
+// 静的コンテンツ
+app.use("/public", express.static(path.join(__dirname, "/public")))
+
+// 動的コンテンツ
 app.use("/", require("./routes/index.js"))
 
-// サーバーの起動
+// サーバーの起動(webアプリの実行)
 app.listen(PORT, ()=> {
     console.log(`App listen ${PORT}`)
 })
