aa4fe69872356e054e5614f51467d27f4a28bd11
tomohiro1024 <gongyezhihong732@gmail.com>
2023-06-28 18:07:02 +0900

    faviconの追加

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const PORT = process.env.PORT
 const path = require("path")
 const express = require("express")
+const favicon = require("serve-favicon")
 const app = express()
 
 // expressのセッティング
 app.set("view engine", "ejs")
 
 // 静的コンテンツ
+app.use(favicon(path.join(__dirname, "/public/favicon.ico")))
 app.use("/public", express.static(path.join(__dirname, "/public")))
 
 // 動的コンテンツ
