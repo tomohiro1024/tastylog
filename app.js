a8f904bc1f44e293144416b9dac03477e590038a
tomohiro1024 <gongyezhihong732@gmail.com>
2023-07-18 14:27:30 +0900

    mysqlに接続し、データを取得

--- app.js
+++ app.js
@@ -26,6 +26,33 @@ app.use("/test", async(req, res, next)=>{
     const { sql } = require("@garafu/mysql-fileloader")({ root: path.join(__dirname, "./lib/database/sql")})
     const config = require("./config/mysql.config.js")
     const mysql = require("mysql")
+    // sql接続
+    const con = mysql.createConnection({
+        host: config.HOST,
+        port: config.PORT,
+        user: config.USERNAME,
+        password: config.PASSWORD,
+        database: config.DATABASE
+    })
+    var data
+    const client = {
+        connect: promisify(con.connect).bind(con),
+        query: promisify(con.query).bind(con),
+        end: promisify(con.end).bind(con)
+    }
+
+    try {
+       await client.connect()
+       data = await client.query(await sql("SELECT_SHOP_BASIC_BY_ID"))
+       console.log(data)
+    } catch (e) {
+        next(e)
+    } finally {
+        // 接続を切断
+        await client.end
+    }
+
+    res.end("OK")
 })
 
 // アプリケーションログ
