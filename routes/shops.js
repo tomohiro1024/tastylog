594641a01c232565bcbd5e93a6d36403460d1e25
tomohiro1024 <gongyezhihong732@gmail.com>
2023-07-28 18:07:26 +0900

    レビューのデータ取得

--- routes/shops.js
+++ routes/shops.js
@@ -8,9 +8,14 @@ router.get("/:id", async (req, res, next) => {
         MySQLClient.executeQuery(
             await sql("SELECT_SHOP_DETAIL_BY_ID"),
             [id]
-        )
+        ),
+        MySQLClient.executeQuery(
+            await sql("SELECT_SHOP_REVIEW_BY_SHOP_ID"),
+            [id]
+        ),
     ]).then((results) => {
         var data = results[0][0]
+        data.reviews = results[1] || [];
         res.render("./shops/index.ejs", data)
     }).catch((e)=>{
         next(e)
